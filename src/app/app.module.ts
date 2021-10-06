d1b64bde02ed200328f7be64967e983e509d8c19
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-10-07 00:33:02 +0200

    primer commit, estructura de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,    
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
